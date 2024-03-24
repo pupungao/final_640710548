0f9bea3f19ba7320cd0af4c7a2844d226498ac7c
Phitsanu <kobeiei22@gmail.com>
2024-03-24 12:19:45 +0700

    2

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ class MyApp2 extends StatefulWidget {
 
 class _MyApp2State extends State<MyApp2> {
   TextEditingController _controller = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+  String _enterUrl = '';
+  String _enterTitle = '';
   List<DataNews> _datanews = [];
 
   void initState() {
@@ -26,9 +29,21 @@ class _MyApp2State extends State<MyApp2> {
     _loadItems();
   }
 
+  void readyOrYet(){
+    if(_controller.text.isEmpty && _descriptionController.text.isEmpty){
+      _Errortryagain();
+    }
+    else{
+      setState(() {
+      _enterUrl = _controller.text;
+      _enterTitle = _descriptionController.text;
+      _Pass();
+    });
+    }
+  }
   Future<void> _loadItems() async {
     try {
-      final data = await ApiCaller().get("datanews");
+      final data = await ApiCaller().get("https://cpsu-api-49b593d4e146.herokuapp.com/api/2_2566/final/web_types");
       List list = jsonDecode(data);
       setState(() {
         _datanews = list.map((e) => DataNews.fromJson(e)).toList();
@@ -41,56 +56,76 @@ class _MyApp2State extends State<MyApp2> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
           appBar: AppBar(
             title: Text('Webby Fondue\nระบบรายงานเว็บเลวๆ'),
             centerTitle: true,
           ),
-          body: Expanded(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Text('* ต้องกรอกข้อมูล'),
-                  MyTextField(
-                    controller: _controller,
-                    hintText: 'URL *',
-                    keyboardType: TextInputType.text,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  MyTextField(
-                    controller: _controller,
-                    hintText: 'รายละเอียด',
-                    keyboardType: TextInputType.text,
-                  ),
-                  Text('ระบุประเภทเว็บเลว *'),
-                  ListView.builder(
-                itemCount: _datanews.length,
-                itemBuilder: (context, index) {
-                  final item = _datanews[index];
-                  return Card(
-                    child: ListTile(
-                      title: Text(item.title),
-                      subtitle: Text(item.subtitle),
-                      trailing: Icon(item.selected ? Icons.check : null),
-                    ),
-                  );
-                },
-              ),
-                ],
-              ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('* ต้องกรอกข้อมูล'),
+                MyTextField(
+                  controller: _controller,
+                  hintText: 'URL *',
+                  keyboardType: TextInputType.text,
+                ),
+                MyTextField(
+                  controller: _descriptionController,
+                  hintText: 'รายละเอียด',
+                  keyboardType: TextInputType.text,
+                ),
+                Text('ระบุประเภทเว็บเลว *'),
+                // ListView.builder(
+                //   itemCount: 5, // Example item count
+                //   itemBuilder: (BuildContext context, int index) {
+                //     DataNews data = _datanews![index];
+                //     return MyListTile(
+                //       title: data.title,
+                //       subtitle: data.subtitle,
+                //       imageUrl: data.imageUrl,
+                //       selected: index == 2, 
+                //       onTap: () {
+                //       },
+                //     );
+                //   },
+                // ),
+                ElevatedButton(
+                  onPressed: readyOrYet, 
+                  child: Text('ส่งข้อมูล')),
+              ],
             ),
-          )),
+          ),
+        ));
+  }
+  Future<void> _Pass() async {
+    try {
+      final data = await ApiCaller().post(
+        "https://cpsu-api-49b593d4e146.herokuapp.com/api/2_2566/final/report_web",
+        params: {
+        },
+      );
+      // API นี้จะส่งข้อมูลที่เรา post ไป กลับมาเป็น JSON object ดังนั้นต้องใช้ Map รับค่าจาก jsonDecode()
+      Map map = jsonDecode(data);
+      String text =
+          'ขอบคุณสำหรับการแจ้งข้อมูล รหัสของคุณคือ ${map['id']} \n\nสถิติการรายงาน \n===== \n เว็บพนัน : ${map['count']}\n เว็บปลอมแปลง เลียนแบบ : ${map['count']}\n เว็บข่าวมั่ว : ${map['count']}\n เว็บแชร์ลูกโซ่ : ${map['count']}\nอื่นๆ : 14';
+      showOkDialog(context: context, title: "Success", message: text);
+    } on Exception catch (e) {
+      showOkDialog(context: context, title: "Error", message: e.toString());
+    }
+  }
+  Future<void> _Errortryagain() async {
+    await showOkDialog(
+      context: context,
+      title: "Error",
+      message: "ต้องกรอก URL",
     );
   }
 }
